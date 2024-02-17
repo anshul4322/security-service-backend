fb816e639c9c2c833c84a57660d5e22116d9497f
anshul4322 <anshulgupta4322@gmail.com>
2024-02-17 19:54:42 +0530

    Attendance_complete

--- src/routes/v1/guardData.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const guardController = require('../../controllers/guard.controller')
-const auth = require("../../middlewares/auth");
-
-const router = express.Router();
-
-router.post("/form", auth, guardController.saveData)
-
-module.exports = router;
